9fc41d7662fc780ae1023faf0c6a094902ee0bfc
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2020-04-12 04:31:24 +1000

    added caching

--- rust-graphql-authoriser/build-lambda.sh
+++ rust-graphql-authoriser/build-lambda.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-alias rust-musl-builder='docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder'
+alias rust-musl-builder='docker run --rm -it -v "$(pwd)":/home/rust/src -v ~/.cargo/registry:/home/rust/.cargo/registry ekidd/rust-musl-builder'
 rust-musl-builder cargo build --release
 zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
